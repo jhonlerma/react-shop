0d44fe0bc7a2cc8079742dadc35ad0ba614d0d60
jhonlerma <john.enfermeria3645@gmail.com>
2023-03-21 11:25:50 -0500

    [ADD] project basic structure

---
+++ webpack.config.dev.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ],
+        alias: {
+
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.(css|sass|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+
+            }
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: 'index.html',
+        }),
+    ],
+    devServer:{
+        static:{
+            directory: path.resolve(__dirname, 'dist'),
+            watch: true
+        },
+        watchFiles: path.join(__dirname, './**'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3033,
+        open: true,
+    }
+
+}
