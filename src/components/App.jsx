4234782530b270ba14f7fc093e0a45cbfbce40af
jhonlerma <john.enfermeria3645@gmail.com>
2023-03-21 18:44:13 -0500

    [ADD] Login container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import Login from '../containers/Login';
+
 import '../styles/global.scss';
 const App = () => {
   return (
-    <div>App</div>
+    <><Login/></>
   )
 }
 
