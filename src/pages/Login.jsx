395a9c2a6e418570dd67c251264d1a47b97d29a6
jhonlerma <john.enfermeria3645@gmail.com>
2023-03-24 18:18:50 -0500

    [ADD] some functionalities

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import { useRef } from 'react';
 import '@styles/Login.scss';
 import LogoYardSale from '@logos/logo_yard_sale.svg'
 
 const Login = () => {
+
+  const form = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form.current)
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password')
+    }
+    console.log(data);
+  }
+
   return (
     <div className="login">
       <div className="form-container">
         <img src={LogoYardSale} alt="logo" className="logo" />
 
-        <form action="/" className="form">
+        <form className="form" ref={form}>
           <label htmlFor="email" className="label">Email address</label>
-          <input type="text" id="email" placeholder="example@mail.com" className="input input-email" />
+          <input type="text" name="email" placeholder="example@mail.com" className="input input-email" />
 
           <label htmlFor="password" className="label">Password</label>
-          <input type="password" id="password" placeholder="********" className="input input-password" />
+          <input type="password" name="password" placeholder="********" className="input input-password" />
 
-          <input type="submit" value="Log in" className="primary-button login-button" />
+          <button value="Log in" className="primary-button login-button" onClick={handleSubmit} />
           <a href="/">Forgot my password</a>
         </form>
 
